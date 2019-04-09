db9ce165f530fcc439ac6cfc0d4854f647427ad4
polinafilacheva <polina.filacheva@gmail.com>
2019-04-09 17:38:06 +0300

    проделела работу с лекции

--- scripts.js
+++ scripts.js
@@ -1,15 +1,55 @@
 const form = document.querySelector('form')
 const tasks = document.querySelector('ul')
+const progress = document.querySelector('.progress')
+
+const WORK_TIME = 10000
 
 function startTimer(e) {
   e.preventDefault()
+  const input = e.target.querySelector('input')
 
-  const.input = e.target.querySelector('input')
-  const listItem = document.createElement('li')
+  input.disabled = true
 
-listItem.innerText = input.value
-  tasks.appendChild(ListItem)
 
+  const key = Date.now()
+
+  localStorage.setItem(key, input.value)
+
+  progress.style.width = '100vw'
+  progress.style.transitionDuration = WORK_TIME + 'ms'
+
+  setTimeout(function () {
+    input.disabled = false
+    input.focus()
+    progress.style.transitionDuration = ''
+    progress.style.width = ''
+
+    const listItem = document.createElement('li')
+    listItem.innerText = input.value
+    tasks.appendChild(listItem)
+    input.value = ''
+  }, WORK_TIME)
 }
 
-form.onsubmit =  LogToConsole
+function loadHistory() {
+
+  const historySize = localStorage.length
+
+
+  if (historySize > 0) {
+
+    for (let i = 0; i < historySize; i++) {
+
+      const key = localStorage.key(i)
+
+      const taskName = localStorage.getItem(key)
+
+      const listItem = document.createElement('li')
+      listItem.innerText = taskName
+      tasks.appendChild(listItem)
+    }
+  }
+}
+
+form.onsubmit = startTimer
+loadHistory()
